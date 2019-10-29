2263dbd5b18a6e534e0173b274d5aa2cdf73f41f
Jim <jbenkejr@gmail.com>
2019-10-28 21:10:51 -0400

    Task List

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace CapstoneTaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PrintOptions();
+                string userInput = GetUserInput("\nPlease choose one of the 5 options: ");
+                SelectMenuOptions(userInput);
+            }
+
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        public static void CreateTask()
+        {
+            Task task = new Task();
+
+            task.Name = GetUserInput("\nEnter Name: ");
+            task.Description = GetUserInput("Enter Description: ");
+            task.DueDate = ValiDate("Enter Due Date: ");
+
+            Task.AddTask(task);
+        }
+
+        public static void RemoveTask()
+        {
+            Task.DisplayTasks();
+
+            string userInput = GetUserInput("\nEnter the number of the task you want to delete: ");
+            int input = int.Parse(userInput);
+
+            string confirm = GetUserInput("\nAre you sure you want to delete this task? (y/n) ").ToLower();
+            if (confirm == "y" || confirm == "yes")
+            {
+                Task.RemoveTask(input-1);
+            }
+        }
+
+        public static void MarkComplete()
+        {
+            Task.DisplayTasks();
+
+            string userInput = GetUserInput("\nEnter Task Number to be marked complete: ");
+            int input = int.Parse(userInput);
+
+            string confirm = GetUserInput("\nAre you sure you want to mark this task completed? (y/n) ").ToLower();
+            if (confirm == "y" || confirm == "yes")
+            {
+                Task.MarkCompleted(input-1);
+            }
+
+           
+        }
+
+        public static DateTime ValiDate(string message)
+        {
+            try
+            {
+                return DateTime.Parse(GetUserInput(message));
+            }
+            catch
+            {
+                Console.WriteLine("Not a valid date");
+                return ValiDate(message);
+            }
+        }
+
+        public static void PrintOptions()
+        {
+            Console.WriteLine("1. List tasks");
+            Console.WriteLine("2. Add task");
+            Console.WriteLine("3. Delete task");
+            Console.WriteLine("4. Mark task complete");
+            Console.WriteLine("5. Quit");
+        }
+
+        public static void SelectMenuOptions(string userInput)
+        {
+            switch (userInput)
+            {
+                case "1":
+                case "List tasks":
+                    Task.DisplayTasks();
+                    break;
+                case "2":
+                case "Add task":
+                    CreateTask();
+                    break;
+                case "3":
+                case "Delete tasks":
+                    RemoveTask();
+                    break;
+                case "4":
+                case "Mark task complete":
+                    MarkComplete();
+                    break;
+                case "5":
+                case "Quit":
+                    Console.WriteLine("\nGoodbye!");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTaskList
+{
+    class Task
+    {
+        private static List<Task> _tasks = new List<Task>
+        {
+            new Task("Harry Potter", "Sweeps the floor", DateTime.Parse("10/28/19")),
+            new Task("Ron Weasley", "Washes the windows", DateTime.Parse("10/29/19")),
+            new Task("Hermione Granger", "Walks the dragon", DateTime.Parse("10/30/19")),
+            new Task("Hagrid", "Bakes cookies", DateTime.Parse("10/31/19"))
+        };
+
+        #region fields
+        private string _name;
+        private string _description;
+        private DateTime _dueDate;
+        private bool _complete;
+        #endregion
+
+        #region properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public DateTime DueDate
+        {
+            get { return _dueDate; }
+            set { _dueDate = value; }
+        }
+        public bool Complete
+        {
+            get { return _complete; }
+            set { _complete = value; }
+        }
+
+        #endregion
+
+        #region constructors
+        public Task()
+        {
+
+        }
+
+        public Task(string name, string description, DateTime dueDate, bool complete = false)
+        {
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+            Complete = complete;
+        }
+
+        public static void DisplayTasks()
+        {
+            for (int i = 0; i < _tasks.Count; i++)
+            {
+                Console.WriteLine($"\n\t{i + 1}. {_tasks[i].Name}");
+                Console.WriteLine($"\tDescription {_tasks[i].Description}");
+                Console.WriteLine($"\tDue Date: {_tasks[i].DueDate.ToShortDateString()}");
+                Console.WriteLine($"\tCompleted? {_tasks[i].Complete}\n");
+            }
+        }
+
+        public static void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+
+        public static void RemoveTask(int userInput)
+        {
+            _tasks.RemoveAt(userInput);
+        }
+
+        public static void MarkCompleted(int userInput)
+        {
+            Task foundTask = _tasks[userInput];
+            foundTask.Complete = true;
+
+        }
+        #endregion
+    }
+}
